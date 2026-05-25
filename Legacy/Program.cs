936aedd0787b26cce195977b1ba359f96d22d6e2
Viktor Ferenczi <viktor@ferenczi.eu>
2026-05-25 03:44:01 +0200

    Loading se-linux-compat as well

--- Legacy/Program.cs
+++ Legacy/Program.cs
@@ -265,7 +265,7 @@ static class Program
     {
         string ds64Dir = ConfigManager.Instance.GameDir;
         bool isGameFramework = Tools.GetFiles(ds64Dir, ["*.config"], []).Any();
-        return isGameFramework ? ["se-dotnet-compat"] : [];
+        return isGameFramework ? ["se-dotnet-compat", "se-linux-compat"] : [];
     }
 
     private static void SetupGameResolver()
